274dd276553840537732139c4c2232af84f8a07e
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2013-10-10 23:43:22 +0200

    Link i admin

--- backend/src/main/java/no/javazone/representations/feedback/AdminFeedback.java
+++ backend/src/main/java/no/javazone/representations/feedback/AdminFeedback.java
@@ -7,6 +7,8 @@ import com.mongodb.DBObject;
 public class AdminFeedback {
 
 	@JsonProperty
+	private final Object id;
+	@JsonProperty
 	private final Object time;
 	@JsonProperty
 	private final Object rating;
@@ -17,7 +19,9 @@ public class AdminFeedback {
 	@JsonProperty
 	private final Object userAgent;
 
-	public AdminFeedback(final Object time, final Object rating, final Object comment, final Object ip, final Object userAgent) {
+	public AdminFeedback(final Object id, final Object time, final Object rating, final Object comment, final Object ip,
+			final Object userAgent) {
+		this.id = id;
 		this.time = time;
 		this.rating = rating;
 		this.comment = comment;
@@ -26,12 +30,13 @@ public class AdminFeedback {
 	}
 
 	public static AdminFeedback fromDbObject(final DBObject o) {
+		Object id = o.get("_id").toString();
 		Object time = o.get("time");
 		Object rating = o.get("rating");
 		Object comment = o.get("comment");
 		Object ip = o.get("ip");
 		Object userAgent = o.get("userAgent");
-		return new AdminFeedback(time, rating, comment, ip, userAgent);
+		return new AdminFeedback(id, time, rating, comment, ip, userAgent);
 	}
 
 }
