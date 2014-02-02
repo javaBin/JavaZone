41b0225c1ed8e1de49632a9f5ac7a0daf90ab659
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2014-02-02 14:17:33 +0100

    Fikser animasjoner i firefox og safari

--- frontend/content/assets/js/main.js
+++ frontend/content/assets/js/main.js
@@ -2,6 +2,7 @@
 
 $(function() {
     jz.utils.addSupportClasses();
+    jz.utils.removeAnimationClasses();
     var path = window.location.pathname.replace(/\/$/, "").split(".")[0];
     var file = _.last(path.split("/")) || "program";
     var name = !(/^[a-z0-9_\-]+$/i).test(file) ? "program" : file;
