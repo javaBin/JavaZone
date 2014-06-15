16a0f3462ef1701698b59835b78183c43771cb71
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2014-06-15 12:59:52 +0200

    Fikser nodeproxy vha gammel versjon av npm-biblioteket

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,6 +7,8 @@
 BASEDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd ${BASEDIR}
 
+npm install http-proxy@0.9.0 colors
+
 cd ../frontend/scripts
 
 npm install underscore semver colors shelljs css glob
