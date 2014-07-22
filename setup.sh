f075c39fc79c82ecf168e61ba4821dd799308167
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2014-07-22 21:27:43 +0200

    Fikser setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ sudo npm install pacbot -g
 
 cd scripts
 npm install http-proxy@0.9.0 colors underscore shelljs semver
+
+cd ../frontend/scripts
+npm install http-proxy@0.9.0 colors underscore shelljs semver
