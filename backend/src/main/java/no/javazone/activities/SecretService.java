4e4bc017b1eecfc81507f1b682d63473c0fde439
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2014-09-21 16:02:51 +0200

    URL-safe secret

--- backend/src/main/java/no/javazone/activities/SecretService.java
+++ backend/src/main/java/no/javazone/activities/SecretService.java
@@ -14,7 +14,7 @@ public class SecretService {
 			String secretPlain = value + PropertiesLoader.getProperty("api.secret");
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
 			byte[] hash = digest.digest(secretPlain.getBytes("UTF-8"));
-			return new String(Base64.encodeBase64(hash));
+			return new String(new Base64(true).encode(hash)).trim();
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
 		} catch (UnsupportedEncodingException e) {
@@ -23,6 +23,10 @@ public class SecretService {
 	}
 
 	public static boolean checkSecret(String id, String secret) {
+//		System.out.println("ID: " + id);
+//		System.out.println("secret: " + secret);
+//		System.out.println("calc: " + getSecretForValue(id));
+//		System.out.println("equals: " + getSecretForValue(id).equals(secret));
 		return getSecretForValue(id).equals(secret);
 	}
 	
