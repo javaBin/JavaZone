4a03b1ccdf164ce438868ebb618484462997faf3
Bendik Solheim <bendik@solheim.im>
2014-11-04 23:05:01 +0100

    Moved header scroll code. Added custom javascript functionality to run functions when the DOM has been loaded without registering to events

--- frontend/content/assets/js/main.js
+++ frontend/content/assets/js/main.js
@@ -1,36 +1,7 @@
 (function() {
 
-	var SCALE_FACTOR = 1;
-	var MIN_ZOOM = .5;
-	var MAX_ZOOM = 1;
-
-	function style(el, style, val) {
-		el.style[style] = val;
-	}
-
-	function scale(el, val) {
-		['WebkitTransform', 'MozTransform', 'OTransform', 'transform'].forEach(function (s) {
-			style(el, s, val);
-		});
-	}
-
-	var menuContainer = document.querySelector('.menu');
-	var logo = document.querySelector('.menu svg');
-	var text = document.querySelector('.menu #javazone-text');
-
-	window.addEventListener('scroll', function() {
-		var scaleFactor = MAX_ZOOM - Math.min(
-			Math.max(MAX_ZOOM, (window.scrollY / SCALE_FACTOR)) / 100, MIN_ZOOM
-		);
-
-		var opacity = 1 - (window.scrollY / 100) * 2;
-		var height = scaleFactor * 98;
-
-		requestAnimationFrame(function() {
-			style(text, 'opacity', opacity);
-			style(logo, 'height', height);
-		});
-
+	window.loaded.forEach(function(f) {
+		f();
 	});
 
 })();
