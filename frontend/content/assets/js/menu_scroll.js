1dc958f56ad1fee2a2e7c0c06aab03e4596019ef
Bendik Solheim <bendik@solheim.im>
2014-11-09 21:43:04 +0100

    Translating menu items towards center when scrolling

--- frontend/content/assets/js/menu_scroll.js
+++ frontend/content/assets/js/menu_scroll.js
@@ -1,14 +1,19 @@
-(function() {
+(function(_) {
 
 	var SCALE_FACTOR = 1;
 	var MIN_ZOOM = .5;
 	var MAX_ZOOM = 1;
 
 	function style(el, style, val) {
-		el.style[style] = val;
+		if (!(el instanceof NodeList))
+			el = [el];
+
+		_(el).each(function(e) {
+			e.style[style] = val;
+		});
 	}
 
-	function scale(el, val) {
+	function transform(el, val) {
 		['WebkitTransform', 'MozTransform', 'OTransform', 'transform'].forEach(function (s) {
 			style(el, s, val);
 		});
@@ -17,6 +22,8 @@
 	var menuContainer = document.querySelector('.nav');
 	var logo = document.querySelector('.nav svg');
 	var text = document.querySelector('.nav #javazone-text');
+	var leftItems = document.querySelectorAll('.nav-item.left a');
+	var rightItems = document.querySelectorAll('.nav-item.right a');
 
 	window.addEventListener('scroll', function() {
 		var scaleFactor = MAX_ZOOM - Math.min(
@@ -25,12 +32,15 @@
 
 		var opacity = 1 - (window.scrollY / 100) * 2;
 		var height = scaleFactor * 98;
+		var translation = 30 - (scaleFactor * 30);
 
 		requestAnimationFrame(function() {
 			style(text, 'opacity', opacity);
 			style(logo, 'height', height);
+			transform(leftItems, 'translateX(' + translation + 'px)');
+			transform(rightItems, 'translateX(-' + translation + 'px)');
 		});
 
 	});
 
-})();
+})(window._);
