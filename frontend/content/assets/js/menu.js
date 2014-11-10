6471f45c30aeda02f1d0c6c17dbe39c4ae7e1b7f
Bendik Solheim <bendik@solheim.im>
2014-11-10 20:48:43 +0100

    Playing around with menu animations. Couldn't resist.

--- frontend/content/assets/js/menu.js
+++ frontend/content/assets/js/menu.js
@@ -1,12 +1,11 @@
-(function(Velocity) {
+(function(Velocity, svgAnimate) {
 
 	var menu = document.querySelector('.nav-toggle');
-	menu.addEventListener('click', toggleMenu, false);
 	var nav = document.querySelector('.nav');
 
 	function toggleMenu() {
 		var top = window.getComputedStyle(nav).top === "0px" ? -80 : 0;
-		Velocity(nav, { top: top }, { duration: 200, easing: 'easeOutExpo' });
+		Velocity(nav, { top: top }, { duration: 400, easing: 'easeInOutCirc' });
 	}
 
 	var mql = window.matchMedia("(min-width: 768px)");
@@ -17,4 +16,69 @@
 			nav.style.top = "-80px";
 	});
 
-})(window.Velocity);
+	var props = {
+		action: toggleMenu,
+		animation: [
+			{
+				el: '.top',
+				animation: {
+					from: {
+						x1: '0',
+						y1: '16px',
+						x2: '30px',
+						y2: '16px',
+					},
+					to: {
+						x1: '4px',
+						y1: '19px',
+						x2: '26px',
+						y2: '1px'
+					}
+				},
+				props: {
+					easing: 'easeInOutCirc',
+					duration: 400
+				}
+			},
+			{
+				el: '.bottom',
+				animation: {
+					from: {
+						x1: '0',
+						y1: '4px',
+						x2: '30px',
+						y2: '4px'
+					},
+					to: {
+						x1: '4px',
+						y1: '1px',
+						x2: '26px',
+						y2: '19px'
+					}
+				},
+				props: {
+					easing: 'easeInOutCirc',
+					duration: 400
+				}
+			},
+			{
+				el: '.mid',
+				animation: {
+					from: {
+						opacity: 1
+					},
+					to: {
+						opacity: 0
+					}
+				},
+				props: {
+					easing: 'easeInOutCirc',
+					duration: 400
+				}
+			}
+		]
+	}
+
+	new svgAnimate(menu, props);
+
+})(window.Velocity, window.svgAnimate);
---
+++ frontend/content/assets/js/svg_animate.js
@@ -0,0 +1,39 @@
+(function(window, Velocity, _) {
+
+	window.svgAnimate = function(el, props) {
+		this.el = el;
+		this.props = props;
+		this.toggled = false;
+		this.animElements = this.getElements();
+		this.registerListener();
+	}
+
+	svgAnimate.prototype.getElements = function() {
+		var _this = this;
+		return this.props.animation.map(function(element) {
+			return {
+				node: _this.el.querySelector(element.el),
+				from: element.animation.from,
+				to: element.animation.to,
+				props: element.props
+			};
+		});
+	};
+
+	svgAnimate.prototype.registerListener = function() {
+		var _this = this;
+		this.el.addEventListener('click', function() { _this.toggle(); }, false);
+	};
+
+	svgAnimate.prototype.toggle = function() {
+		var to = this.toggled ? 'from' : 'to';
+		this.animElements.forEach(function(element) {
+			var props = _.defaults({}, element.props, { duration: 400, easing: 'easeOutExpo' })
+			Velocity(element.node, element[to], props);
+		});
+
+		this.toggled = !this.toggled;
+		this.props.action && this.props.action();
+	};
+
+})(window, window.Velocity, window._);
