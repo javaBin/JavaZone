ff36ded6393a2af4bba10e566426928b68cf4ffc
Bendik Solheim <bendik@solheim.im>
2014-11-12 19:52:29 +0100

    Revert "Finally got Less working. Moved colors into vars.less. Started transforming css files"
    This reverts commit 81f8aefb506595e924a0069277b1ca3ebdba52ce.

--- frontend/pacbot.js
+++ frontend/pacbot.js
@@ -1,18 +1,8 @@
 /*global exports */
 
-var less = require('less');
-var fs = require('fs');
-
 exports.config = function(pacbot) {
 
-	var config = {
-		port: 3000,
-		ignore_build: ["bower_components"],
-		ignore_processing: ["assets/templates", "assets/components"],
-		assets: {}
-	};
-
-	config.assets.js = {
+	var js = {
 		all: [
 			"bower_components/lodash/dist/lodash.min.js",
 			"bower_components/velocity/velocity.min.js",
@@ -22,31 +12,23 @@ exports.config = function(pacbot) {
 		]
 	}
 
-	config.assets.css = {
+	var css = {
 		all: [
 			"assets/css/reset.css",
 			"assets/css/base.css",
+			"assets/css/components.css",
 			"assets/css/teaser.css",
+			"assets/css/menu.css",
 			"assets/css/interview.css"
 		]
 	};
 
-	config.assets.less = {
-		all: [
-			"assets/less/base.less",
-			"assets/less/components.less",
-			"assets/less/menu.less",
-		]
-	};
-
-	/*pacbot.filter.set('compile', 'less', function(file, data, locals, callback) {
-	    var file_content = fs.readFileSync(file).toString();
-	    less.render(file_content, function (err, css) {
-	        if (err) log('error', 'could not render less file', file);
-	        if (err) throw(err);
-	        callback(css.css);
-	    });
-	});*/
-
-	return config;
+	return {
+		assets: {
+			js: js,
+			css: css
+		},
+		ignore_build: ["bower_components"],
+		ignore_processing: ["assets/templates", "assets/components"]
+	}
 }
