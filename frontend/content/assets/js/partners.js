08bed5166ba3d8345e6332b91781a4e322440ae5
Bendik Solheim <bendik@solheim.im>
2014-11-18 23:48:24 +0100

    Trying some small animations

--- frontend/content/assets/js/partners.js
+++ frontend/content/assets/js/partners.js
@@ -11,11 +11,14 @@
 
 	jz.partners = function() {
 		var imagePath = '/assets/img/partners/';
-		var partners =document.querySelector('.partner-list');
-		partners.innerHTML = '<ul class="cf">' +  _(jz.data.partners).shuffle().map(function(partner) {
-			//return '<li><a href="' + partner[2] + '"><img src="' + imagePath + partner[1] + '" alt="' + partner[0] + '"></a><li>';
-			return String.format('<li><a href="{0}"><img src="{1}" alt="{2}"></a>', partner[2], imagePath + partner[1], partner[0]);
-		}).join('') + '</ul>'
+		var partners = document.querySelector('.partner-list');
+		var partnerList = _(jz.data.partners)
+			.shuffle()
+			.map(function(partner) {
+				return String.format('<li><a href="{0}"><img src="{1}" alt="{2}"></a>', partner[2], imagePath + partner[1], partner[0]);
+			})
+			.join('');
+		partners.innerHTML = String.format('<ul class="cf">{0}</ul>', partnerList);
 	}
 
 })(window._, window.jz = window.jz || {});
