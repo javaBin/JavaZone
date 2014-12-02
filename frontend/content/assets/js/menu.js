b89a2666ccb4e1bae49f3313f17354388087bb58
Bendik Solheim <bendik@solheim.im>
2014-12-02 22:02:41 +0100

    Rewritten to not do anything without invocation

--- frontend/content/assets/js/menu.js
+++ frontend/content/assets/js/menu.js
@@ -1,89 +1,92 @@
 (function(Velocity, svgAnimate, jz) {
 
-	(function setActive() {
+	jz.menu = {};
+
+	jz.menu.setActive = function() {
 		var menuItems = document.querySelectorAll('.nav .nav-item a');
 		var active = _(menuItems).reduce(function(acc, a) {
 			return a.pathname === location.pathname ? a : acc;
 		}, null);
 		if (active)
 			active.classList.add('active');
-	})();
+	};
 
-	var nav = document.querySelector('.nav');
 
-	var mql = window.matchMedia("(min-width: 768px)");
-	mql.addListener(function(mql) {
-		if (mql.matches)
-			nav.style.top = 0;
-		else
-			nav.style.top = "-96px";
-	});
-
-	var props = {
-		action: function() {
-			var top = window.getComputedStyle(nav).top === "0px" ? -96 : 0;
-			Velocity(nav, { top: top }, { duration: 400, easing: 'easeInOutCirc' });
-		},
-		animation: [
-			{
-				el: '.top',
-				animation: {
-					from: {
-						x1: '0',
-						y1: '16px',
-						x2: '30px',
-						y2: '16px',
-					},
-					to: {
-						x1: '4px',
-						y1: '19px',
-						x2: '26px',
-						y2: '1px'
-					}
-				},
-				props: {
-					easing: 'easeInOutCirc',
-					duration: 400
-				}
+	jz.menu.initializeMenu = function() {
+		var nav = document.querySelector('.nav');
+		var mql = window.matchMedia("(min-width: 768px)");
+		mql.addListener(function(mql) {
+			if (mql.matches)
+				nav.style.top = 0;
+			else
+				nav.style.top = "-96px";
+		});
+		var props = {
+			action: function() {
+				var top = window.getComputedStyle(nav).top === "0px" ? -96 : 0;
+				Velocity(nav, { top: top }, { duration: 400, easing: 'easeInOutCirc' });
 			},
-			{
-				el: '.bottom',
-				animation: {
-					from: {
-						x1: '0',
-						y1: '4px',
-						x2: '30px',
-						y2: '4px'
+			animation: [
+				{
+					el: '.top',
+					animation: {
+						from: {
+							x1: '0',
+							y1: '16px',
+							x2: '30px',
+							y2: '16px',
+						},
+						to: {
+							x1: '4px',
+							y1: '19px',
+							x2: '26px',
+							y2: '1px'
+						}
 					},
-					to: {
-						x1: '4px',
-						y1: '1px',
-						x2: '26px',
-						y2: '19px'
+					props: {
+						easing: 'easeInOutCirc',
+						duration: 400
 					}
 				},
-				props: {
-					easing: 'easeInOutCirc',
-					duration: 400
-				}
-			},
-			{
-				el: '.mid',
-				animation: {
-					from: {
-						opacity: 1
+				{
+					el: '.bottom',
+					animation: {
+						from: {
+							x1: '0',
+							y1: '4px',
+							x2: '30px',
+							y2: '4px'
+						},
+						to: {
+							x1: '4px',
+							y1: '1px',
+							x2: '26px',
+							y2: '19px'
+						}
 					},
-					to: {
-						opacity: 0
+					props: {
+						easing: 'easeInOutCirc',
+						duration: 400
 					}
 				},
-				props: {
-					easing: 'easeInOutCirc',
-					duration: 400
+				{
+					el: '.mid',
+					animation: {
+						from: {
+							opacity: 1
+						},
+						to: {
+							opacity: 0
+						}
+					},
+					props: {
+						easing: 'easeInOutCirc',
+						duration: 400
+					}
 				}
-			}
-		]
+			]
+		}
+		new svgAnimate(document.querySelector('.nav-toggle'), props);
 	}
-	new svgAnimate(document.querySelector('.nav-toggle'), props);
 
 })(window.Velocity, window.svgAnimate, window.jz = window.jz || {});
