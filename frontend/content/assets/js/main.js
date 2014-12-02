6a055246253b5dd236666e9bd369a2695531e9dd
Bendik Solheim <bendik@solheim.im>
2014-12-02 22:01:42 +0100

    Supporting N functions per route

--- frontend/content/assets/js/main.js
+++ frontend/content/assets/js/main.js
@@ -1,12 +1,19 @@
 (function(_, jz) {
+	"use strict";
 
 	var routes = {
+		'.*': [jz.menu.setActive, jz.menu.initializeMenu],
 		'partners': jz.partners
 	};
 
 	_(Object.keys(routes)).each(function(route) {
-		if (new RegExp(route).test(location.pathname))
-			routes[route]();
+		console.log(new RegExp(route).test(location.pathname))
+		if (new RegExp(route).test(location.pathname)) {
+			if (typeof routes[route] === 'function')
+				routes[route]();
+			else
+				_.invoke(routes[route], Function.prototype.apply);
+		}
 	});
 
 })(window._, window.jz = window.jz || {});
