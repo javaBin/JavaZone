af2eef2ac1c6da00ded449a143be59c20919f8bf
Bendik Solheim <bendik@solheim.im>
2015-06-07 12:35:24 +0200

    Now able to list all talks

--- frontend/content/assets/js/program.js
+++ frontend/content/assets/js/program.js
@@ -1,17 +1,29 @@
-(function(_, request, jz) {
+(function(_, request, Handlebars, jz) {
 
-	function program() {
-		request('http://test.javazone.no/javazone-web-api/event/javazone_2014/sessions')
-		.end(function(err, res) {
-			if (err) {
-				console.log(err);
-				return;
-			}
+    _.templateSettings.variable = 'data';
 
-			console.log(res);
-		});
-	}
+    function program() {
+        request('http://test.javazone.no/javazone-web-api/event/javazone_2014/sessions')
+        .end(create);
+    }
+
+    function create(err, res) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log(res);
+
+        var data = JSON.parse(res.text);
+        var template = Handlebars.compile(document.querySelector('.program-template').innerHTML);
+        var program = document.querySelector('.javazone-program');
+
+        console.log(data);
+
+        program.innerHTML = template({talks: data});
+    }
 
 	jz.program = program;
 
-})(window._, window.superagent, window.jz = window.jz || {});
+})(window._, window.superagent, window.Handlebars, window.jz = window.jz || {});
