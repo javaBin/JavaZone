d6c7e918c232b662c0f0b06cbbdd4288e5ebd732
Bendik Solheim <bendik@solheim.im>
2015-08-13 01:01:28 +0200

    Playable video

--- frontend/content/assets/js/index.js
+++ frontend/content/assets/js/index.js
@@ -8,6 +8,8 @@
 
         countdown(movie2, release2);
         countdown(movie3, release3);
+
+        attachClickListeners();
     }
 
     function countdown(el, goal) {
@@ -38,6 +40,38 @@
             minutes: Math.floor(d / 60000 % 60),
             seconds: Math.floor(d / 1000 % 60)
         };
+    }
+
+    function attachClickListeners() {
+        var movie1 = document.querySelector('.movie-1 .play');
+        movie1.addEventListener('click', function() {
+            var movieContainer = document.querySelector('.movie-container');
+            Velocity(movieContainer, 'slideDown', {
+                display: 'inline-block',
+                duration: 250,
+                complete: startMovie
+            });
+        });
+    }
+
+    function startMovie() {
+        var tag = document.createElement('script');
+        tag.src = 'http://www.youtube.com/player_api';
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        window.onYouTubePlayerAPIReady = function() {
+            var player = new YT.Player('movie', {
+                height: 480,
+                width: 640,
+                videoId: '3vI_7os2V_o',
+                events: {
+                    'onReady': function(event) {
+                        event.target.playVideo();
+                    }
+                }
+            })
+        }
     }
 
 })(window.jz = window.jz || {});
