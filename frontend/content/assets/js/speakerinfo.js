cb968c982ea913571510099d82e6d6034eff4134
Bendik Solheim <bendik@solheim.im>
2015-08-22 14:29:28 +0200

    Added a mood image to speakerinfo page

--- frontend/content/assets/js/speakerinfo.js
+++ frontend/content/assets/js/speakerinfo.js
@@ -1,6 +1,7 @@
 (function(jz, _, Velocity) {
 
     jz.speakerinfo = function() {
+        attachScrollListener();
         createRoomWidget();
         showRoom('room-1');
     };
@@ -44,6 +45,19 @@
                 room.classList.add('active');
             }
         });
+    }
+
+    function attachScrollListener() {
+        var nav = document.querySelector('.nav')
+        document.addEventListener('scroll', _.throttle(function() {
+            var height = document.querySelector('.mood').clientHeight;
+            var scrolled = document.body.scrollTop;
+            if (scrolled > height && !nav.classList.contains('bg')) {
+            nav.classList.add('bg');
+            } else if (scrolled < height) {
+            nav.classList.remove('bg');
+            }
+        }, 100));
     }
 
 })(window.jz = window.jz || {}, window._, window.Velocity);
