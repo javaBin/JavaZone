1b88a0b50903049cea639a010cf6377de03cf6e7
Bendik Solheim <bendik@solheim.im>
2015-10-01 22:26:51 +0200

    Correct base url

--- frontend/content/assets/js/data.js
+++ frontend/content/assets/js/data.js
@@ -112,8 +112,8 @@
         ["Hans Ove Ringstad", "/assets/img/javabin/hans_ove_ringstad.jpg", "#"]
     ];
 
-    //var baseUrl = 'http://javazone.no/javazone-web-api/events/javazone_2015/sessions';
-    var baseUrl = 'http://test.javazone.no/javazone-web-api/events/javazone_2015/sessions';
+    var baseUrl = 'http://javazone.no/javazone-web-api/events/javazone_2015/sessions';
+    //var baseUrl = 'http://test.javazone.no/javazone-web-api/events/javazone_2015/sessions';
     //var baseUrl = 'http://dev.javazone.no/resources/program.json';
 
     jz.data.program = function() {
