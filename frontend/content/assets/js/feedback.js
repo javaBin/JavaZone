7eac8625dfe720ef267f709065497fb8169d407b
Bendik Solheim <bendik@solheim.im>
2015-10-04 22:54:56 +0200

    Added bars showing online feedback as well. Page is more or less complete now

--- frontend/content/assets/js/feedback.js
+++ frontend/content/assets/js/feedback.js
@@ -16,6 +16,28 @@
         };
     }
 
+    function fixOnline(online) {
+        return {
+            overall: {
+                percentage: online.overall * 20,
+                value: online.overall.toFixed(1)
+            },
+            content: {
+                percentage: online.content * 20,
+                value: online.content.toFixed(1)
+            },
+            quality: {
+                percentage: online.quality * 20,
+                value: online.quality.toFixed(1)
+            },
+            relevance: {
+                percentage: online.relevance * 20,
+                value: online.relevance.toFixed(1)
+            },
+            count: online.count
+        };
+    }
+
     function getTalk() {
         id = decodeURIComponent(location.search.substr(1).split('=')[1]);
         jz.data.talk(id)
@@ -38,8 +60,17 @@
             title: submission.tittel,
             speaker: _.pluck(submission.foredragsholdere, 'navn').join(', '),
             paper: {
-                session: feedback.session.paper,
+                session: {
+                    red: feedback.session.paper.red,
+                    yellow: feedback.session.paper.yellow,
+                    green: feedback.session.paper.green,
+                    total: feedback.session.paper.red + feedback.session.paper.yellow + feedback.session.paper.green
+                },
                 conference: fixPaper(feedback.conference.paper)
+            },
+            online: {
+                session: fixOnline(feedback.session.online),
+                conference: fixOnline(feedback.conference.online)
             },
             hasParticipants: (feedback.session.participants > 0 && feedback.session.participants < 1000),
             participants: feedback.session.participants,
